da6b2510a4dad3f594d800831d4c326e63159e81
TanoliSaqib276 <tanolisaqib276@gmail.com>
2021-08-03 17:41:26 +0500

    third half

--- app.js
+++ app.js
@@ -1,29 +1,86 @@
 let itemList = document.getElementById("list-item");
+
 let orderedList = document.getElementById("itemInList");
-function add(){
-    
+
+
+function add() {
+
     let li = document.createElement('li');
     let txt = document.createElement('h3');
     let inputTxt = document.createTextNode(itemList.value);
     txt.appendChild(inputTxt);
     li.appendChild(txt);
-    orderedList.appendChild(li);
-//for Delete button
+    orderedList.appendChild(li);// IN WHOLE SCENRIO ONLY LI WILL BE APPEND IN UL 
+    itemList.value = '';
+    //for Delete button
     let delbtn = document.createElement('button');
     let btnTxt = document.createTextNode('DELETE');
     delbtn.appendChild(btnTxt);
-    delbtn.setAttribute('class','btn')
-    orderedList.appendChild(delbtn);
-//For Edit btn
+    delbtn.setAttribute('class', 'btn');
+    delbtn.setAttribute('onclick', 'delDom(this)');
+    li.appendChild(delbtn)
+    // orderedList.appendChild(delbtn); // WRONG STEP
+    //For Edit btn
     let editbtn = document.createElement('button');
     let edtbtnTxt = document.createTextNode('EDIT');
     editbtn.appendChild(edtbtnTxt);
-    editbtn.setAttribute('class','btn')
-    orderedList.appendChild(editbtn)
+    editbtn.setAttribute('class', 'btn')
+    editbtn.setAttribute('onclick', 'editDom(this)')
+    // orderedList.appendChild(editbtn) //WRONG STEP
+    li.appendChild(editbtn)
+
+}
+function delAll() {
+    
+    orderedList.innerHTML = '';
+}
+
+function delDom(delElement){
+    let remList = delElement.parentNode
+    
+    remList.remove()
     
 }
-// function delAll(){
-//     let trgToRemove = document.getElementById('itemInList').childNodes[2];
-//     console.log(trgToRemove)
-// }
-function edt(){}
+
+let addBtn = document.getElementById('add');
+
+function editDom(editElement){
+    // console.log(editElement);
+
+    // let toEdit = editElement.parentNode.firstChild.innerHTML;
+
+    // console.log(toEdit);
+
+    // let newTxt = prompt('write updated txt')
+
+    // editElement.parentNode.firstChild.innerHTML = newTxt
+
+    
+    let editedText = editElement.parentNode.firstChild.innerHTML;
+    
+    itemList.value = editedText;
+    addBtn.innerHTML = 'Update';
+    addBtn.setAttribute('onclick','updateItem(this)');
+    
+}
+
+function updateItem(updateElement){
+
+    console.log(updateElement,itemList.value);
+
+    let parentToFind = orderedList.childNodes;
+
+    console.log(parentToFind);
+
+    // orderedList.childNodes[1].childNodes[0].innerHTML = itemList.value;
+
+    // addBtn.innerHTML = 'ADD';
+
+    // itemList.value = '';
+
+
+}
+
+
+
+
